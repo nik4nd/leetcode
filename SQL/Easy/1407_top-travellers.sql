d86c9d57357437d873e4d93bf9eb5a9f884aae8e
nik4nd <nik4nd@gmail.com>
2022-08-17 19:05:47 +0300

    Add a solution to Problem 1407

---
+++ SQL/Easy/1407_top-travellers.sql
@@ -0,0 +1,5 @@
+SELECT u.name, IFNULL(SUM(r.distance), 0) AS travelled_distance
+FROM Users AS u LEFT JOIN Rides AS r
+ON u.id = r.user_id
+GROUP BY r.user_id
+ORDER BY SUM(r.distance) DESC, u.name;
